08d69a033c1812f33094fbcaedc64c423946f667
Ravi Upadhyay <ravi.upadhyay@ing.com>
2023-04-12 02:59:21 +0200

    WIP: readFileStream.py, read one line after every 2 seconds

--- backendService/readFileStream.py
+++ backendService/readFileStream.py
@@ -1,11 +1,21 @@
-
+from time import sleep
 
 DEFAULT_ENCODING = "utf-8"
 FILE_LOCATION = "./streamMock/meetup.txt"
+SLEEP_TIME_BEFORE_READ_NEXT_LINE = 2
 
-f = open(FILE_LOCATION, mode="rt", encoding=DEFAULT_ENCODING)
-line = f.readline()
-print(line)
+# f = open(FILE_LOCATION, mode="rt", encoding=DEFAULT_ENCODING)
+# line = f.readline()
+# print(line)
+
+def processLine(l):
+    print(l)
+    sleep(SLEEP_TIME_BEFORE_READ_NEXT_LINE)
+
+
+with open(FILE_LOCATION, mode="rt", encoding=DEFAULT_ENCODING) as openfileobject:
+    for line in openfileobject:
+        processLine(line)
 
 # def openFileStream(): 
 #     return open(FILE_LOCATION, mode="rt", encoding=DEFAULT_ENCODING)
